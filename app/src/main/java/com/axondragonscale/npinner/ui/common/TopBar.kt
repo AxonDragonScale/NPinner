f5a5782b63b3310e5a146379c59fcf562adf52e4
Ronak Harkhani <ronak.harkhani@phonepe.com>
2023-04-27 01:12:51 +0530

    TopBar and BottomBar

---
+++ app/src/main/java/com/axondragonscale/npinner/ui/common/TopBar.kt
@@ -0,0 +1,60 @@
+package com.axondragonscale.npinner.ui.common
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.axondragonscale.npinner.ui.theme.NPinnerTheme
+
+/**
+ * Created by Ronak Harkhani on 26/04/23
+ */
+
+private val TOP_BAR_HEIGHT = 56.dp
+
+@Composable
+fun TopBar(
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(TOP_BAR_HEIGHT)
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .align(Alignment.BottomCenter)
+                .background(MaterialTheme.colorScheme.onBackground.copy(alpha = 0.15F))
+        )
+    }
+}
+
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(name = "Dark Mode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun TopBarPreview() {
+    NPinnerTheme {
+        TopBar("Top Bar")
+    }
+}
